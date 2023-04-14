b0e6aa1ff267645a11e36a907259be716ded4357
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-14 20:53:13 +0200

    Implement Constructor Dependency Injection for TaskDetailViewModel

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailViewModel.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailViewModel.kt
@@ -24,17 +24,13 @@ import com.example.android.architecture.blueprints.todoapp.data.Result
 import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import kotlinx.coroutines.launch
 
 /**
  * ViewModel for the Details screen.
  */
-class TaskDetailViewModel(application: Application) : AndroidViewModel(application) {
-
-    // Note, for testing and architecture purposes, it's bad practice to construct the repository
-    // here. We'll show you how to fix this during the codelab
-    private val tasksRepository = DefaultTasksRepository.getRepository(application)
-
+class TaskDetailViewModel(private val tasksRepository: TasksRepository) : ViewModel() {
     private val _taskId = MutableLiveData<String>()
 
     private val _task = _taskId.switchMap { taskId ->
@@ -115,5 +111,20 @@ class TaskDetailViewModel(application: Application) : AndroidViewModel(applicati
 
     private fun showSnackbarMessage(@StringRes message: Int) {
         _snackbarText.value = Event(message)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    class TaskDetailViewModelFactory(private val repository: TasksRepository) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(TaskDetailViewModel::class.java))
+                return TaskDetailViewModel(repository) as T
+            else
+                throw IllegalArgumentException(
+                    "Unknown ViewModel class. Expected ${this@TaskDetailViewModelFactory::class.java.simpleName}"
+                            + " but ${modelClass::class.java.simpleName} found"
+                )
+        }
+
     }
 }
