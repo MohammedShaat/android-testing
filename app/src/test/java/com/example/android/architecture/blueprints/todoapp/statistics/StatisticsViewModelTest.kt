0b64ccba1f2e56b03dda6ddeb6ab3ba7163a8b60
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-16 17:37:17 +0200

    Test coroutine timing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -4,7 +4,9 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -30,6 +32,13 @@ class StatisticsViewModelTest {
     }
 
     @Test
-    fun testing() {}
+    fun refresh_refreshesTasksAndShowIndicator() {
+        val indicator = viewModel.dataLoading
+        mainCoroutineRule.pauseDispatcher()
+        viewModel.refresh()
+        assertThat(indicator.getOrAwaitValue(), `is`(true))
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(indicator.getOrAwaitValue(), `is`(false))
+    }
 
 }
